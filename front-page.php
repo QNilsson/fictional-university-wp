2b5a8c2b0cd75f34bf820534ad70500f170057c9
Quinn <quinn.nilsson@gmail.com>
2021-05-14 16:15:23 -0600

    events class active

--- front-page.php
+++ front-page.php
@@ -32,47 +32,55 @@
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a
               href="<?php the_permalink(); ?>"><?php the_title()?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18)?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn More</a></p>
+          <p><?php if(has_excerpt()){
+          echo get_the_excerpt();
+        }else{
+          echo wp_trim_words(get_the_content(), 18);
+        }?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn More</a></p>
         </div>
       </div>
       <?php   }
       ?>
 
-    <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event')?>" class="btn btn--blue">View All Events</a></p>
+    </div>
   </div>
-</div>
-<div class="full-width-split__two">
-  <div class="full-width-split__inner">
-    <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-    <?php 
+  <div class="full-width-split__two">
+    <div class="full-width-split__inner">
+      <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+      <?php 
         $homepagePosts = new WP_Query(array(
           'posts_per_page' => 2,
         ));
           while($homepagePosts->have_posts()){
            $homepagePosts->the_post();?>
-    <div class="event-summary">
-      <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink()?>">
-        <span class="event-summary__month"><?php the_time('M')?></span>
-        <span class="event-summary__day"><?php the_time('d')?></span>
-      </a>
-      <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a
-            href="<?php the_permalink()?>"><?php the_title()?></a></h5>
-        <p><?php echo wp_trim_words(get_the_content(), 18)?> <a href="<?php the_permalink()?>" class="nu gray">Read
-            more</a></p>
+      <div class="event-summary">
+        <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink()?>">
+          <span class="event-summary__month"><?php the_time('M')?></span>
+          <span class="event-summary__day"><?php the_time('d')?></span>
+        </a>
+        <div class="event-summary__content">
+          <h5 class="event-summary__title headline headline--tiny"><a
+              href="<?php the_permalink()?>"><?php the_title()?></a></h5>
+          <p><?php if(has_excerpt()){
+          echo get_the_excerpt();
+        }else{
+          echo wp_trim_words(get_the_content(), 18);
+        }?> <a href="<?php the_permalink()?>" class="nu gray">Read
+              more</a></p>
+        </div>
       </div>
-    </div>
-    <?php  }
+      <?php  }
       wp_reset_postdata();
           ?>
 
 
 
 
-    <p class="t-center no-margin"><a href="<?php echo site_url('/blog');?>" class="btn btn--yellow">View All Blog
-        Posts</a></p>
+      <p class="t-center no-margin"><a href="<?php echo site_url('/blog');?>" class="btn btn--yellow">View All Blog
+          Posts</a></p>
+    </div>
   </div>
-</div>
 </div>
 
 <div class="hero-slider">
